1e003d6031101b1ae96afb6514e0442e7674464b
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-13 18:19:50 +0200

    Fixed the error mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ export default function App() {
     const fetchPosts = async () => {
       try {
         const response = await fetch(
-          "https://jsonplaceholder.typicode.com/posts"
+          "https://jsonplaceholderg.typicode.com/posts"
         );
         if (!response.ok) {
-          throw new Error(error);
+          throw new Error("Failed to fetch posts");
         }
         const data = await response.json();
         setPosts(data);
@@ -27,33 +27,24 @@ export default function App() {
   if (error) {
     return (
       <div className="error-message">
-        {<img src="./images/error-message.png" />}
+        <h1>Data fetching failed</h1>
       </div>
     );
   }
 
-return (  
-  <div className="blog-posts">
-      {posts.map((post, index) => (
-        <div key={post.id} className="post">
-          <h2>
-            {index + 1}.{post.title}
-          </h2>
-          <p>{post.body}</p>
-        </div>
-      ))}
+  return (
+    <div className="app-container">
+      <h1>Posts</h1>
+      <div className="blog-posts">
+        {posts.map((post, index) => (
+          <div key={post.id} className="post">
+            <h2>
+              {index + 1}.{post.title}
+            </h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-
-};
-
-    
-
-
-  
-    return (
-      
-    );
-  
-
-  
+}
