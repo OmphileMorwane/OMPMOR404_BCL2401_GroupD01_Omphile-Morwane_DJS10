d0a23e32d7b36bbfda4515d3079e654bc07e17c3
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-14 08:51:23 +0200

    Added documentation to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,37 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 export default function App() {
+  // State variables for storing posts and error message
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
 
+  //useEffectr hook to fetch data from the API when the component mounts
   useEffect(() => {
     const fetchPosts = async () => {
       try {
+        // Fetch blog posts from JSONPlaceholder API
         const response = await fetch(
-          "https://jsonplaceholderg.typicode.com/posts"
+          "https://jsonplaceholder.typicode.com/posts"
         );
+        // Check if response is not successful, throw error if so
         if (!response.ok) {
           throw new Error("Failed to fetch posts");
         }
+        //Parse response data into JSON format
         const data = await response.json();
+
+        //Update posts state with fetched data
         setPosts(data);
       } catch (error) {
+        // Catch any errors that occur during the fetch process
         setError(error.message);
       }
     };
-
+    // Call the fetchPosts function when components mounts
     fetchPosts();
-  }, []);
+  }, []); // Empty dependency array ensures useEffect runs only once on mount
 
+// Render error message if error state is not null
   if (error) {
     return (
       <div className="error-message">
@@ -31,16 +40,18 @@ export default function App() {
       </div>
     );
   }
-
+// Render posts if no error occurred
   return (
     <div className="app-container">
-      <h1>Posts</h1>
+      <h1>Posts</h1> {/*Main heading for the blog posts*/}
       <div className="blog-posts">
         {posts.map((post, index) => (
           <div key={post.id} className="post">
+            {/* Display the post title with a sequential number*/}
             <h2>
               {index + 1}.{post.title}
             </h2>
+            {/* Display the post body */}
             <p>{post.body}</p>
           </div>
         ))}
